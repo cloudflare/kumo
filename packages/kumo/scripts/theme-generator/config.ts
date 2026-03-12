6974cd1669937f708f594fc4b0c14a4ec1865fab
Najla S <you@example.com>
2026-03-12 16:05:35 -0700

    fix(theme): updated ring color in both light & dark mode

--- packages/kumo/scripts/theme-generator/config.ts
+++ packages/kumo/scripts/theme-generator/config.ts
@@ -261,7 +261,7 @@ export const THEME_CONFIG: ThemeConfig = {
       newName: "",
       theme: {
         kumo: {
-          light: "var(--color-kumo-neutral-450, oklch(86% 0 0))",
+          light: "var(--color-kumo-neutral-150, oklch(93.5% 0 0))",
           dark: "var(--color-neutral-700, oklch(37.1% 0 0))",
         },
         fedramp: {
