4777173222ad8a42e90d857cb4eeeaad85a533b7
Matt Rothenberg <mrothenberg@cloudflare.com>
2026-08-13 12:03:45 -0400

    chore: format sidebar mobile hook

--- packages/kumo/src/components/sidebar/sidebar.tsx
+++ packages/kumo/src/components/sidebar/sidebar.tsx
@@ -107,11 +107,14 @@ const FOCUSABLE_SELECTOR =
 function useIsMobile(breakpoint: number = MOBILE_BREAKPOINT) {
   const query = `(max-width: ${breakpoint - 1}px)`;
 
-  const subscribe = useCallback((callback: () => void) => {
-    const mql = window.matchMedia(query);
-    mql.addEventListener("change", callback);
-    return () => mql.removeEventListener("change", callback);
-  }, [query]);
+  const subscribe = useCallback(
+    (callback: () => void) => {
+      const mql = window.matchMedia(query);
+      mql.addEventListener("change", callback);
+      return () => mql.removeEventListener("change", callback);
+    },
+    [query],
+  );
 
   const getSnapshot = useCallback(
     () => window.matchMedia(query).matches,
